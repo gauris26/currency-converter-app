87f02eb1b371fae8da27de321a4f96df0c7a0bc8
Gauris Javier <garu_gauris@hotmail.com>
2024-11-08 20:28:04 -0400

    Implement Testing Deploy

---
+++ src/App.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import App from './App';
+
+test('renders welcome message', () => {
+    render(<App />);
+    const welcomeMessage = screen.getByText(/Bienvenido a mi Aplicacion React con Typescript/i);
+    expect(welcomeMessage).toBeInTheDocument();
+});
+
+test('toggles message when button is clicked', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /Mostrar Mensaje/i });
+
+    // Simular el clic en el botón
+    fireEvent.click(button);
+
+    // Verificar que el mensaje aparece
+    const message = screen.getByText(/Felicidades! Tu aplicacion se encuentra desplegada y corriendo/i);
+    expect(message).toBeInTheDocument();
+
+    // Simular otro clic para ocultar el mensaje
+    fireEvent.click(button);
+    expect(message).not.toBeInTheDocument();
+});
